09b2d0e0a7e4a03d1c29eee99bc8e5c482477bf1
Marco Zagaria <marcozagaria@live.com>
2024-11-12 16:49:58 +0100

    modifiche

--- BW_CRM/src/main/java/team_3/BW_CRM/repositories/ClienteRepository.java
+++ BW_CRM/src/main/java/team_3/BW_CRM/repositories/ClienteRepository.java
@@ -4,6 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import team_3.BW_CRM.entities.Cliente;
 
+import java.util.Optional;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente,Long> {
+    Optional<Cliente> findByEmail(String email);
+
+    Optional<Cliente> findByPartitaIva(String partitaIva);
+
+    Optional<Cliente> findByRagioneSociale(String ragioneSociale);
 }
